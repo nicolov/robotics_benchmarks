1054e13f89424efa00413b7fbd3ac1a9cb35248f
nicolov <nicolov@users.noreply.github.com>
2020-02-05 00:11:33 +0100

    Add benchmark for romeric/Fastor

--- src/kalman.cc
+++ src/kalman.cc
@@ -1,9 +1,13 @@
-#include <benchmark/benchmark.h>
 #include <Eigen/Dense>
+#include <Fastor/Fastor.h>
+#include <benchmark/benchmark.h>
 
 #include <algorithm>
 #include <iostream>
+#include <random>
 #include <vector>
+
+std::uniform_int_distribution<int> int_distribution(0, 100);
 
 template <int CStateSize, int CObsSize = 3, typename NumType = double>
 static void CovarianceUpdateEigen(benchmark::State &state) {
@@ -18,18 +22,23 @@ static void CovarianceUpdateEigen(benchmark::State &state) {
   const int obsSize = CObsSize;
 
   Matrix<NumType, CStateSize, CStateSize> P_x;  // covariance matrix
-  // Random symmetric matrix for the initial covariance.
-  P_x = Matrix<NumType, Dynamic, Dynamic>::Random(stateSize, stateSize);
+  P_x.resize(stateSize, stateSize);
+  P_x.setOnes();
   P_x = (P_x.transpose() * P_x).eval();
 
   Matrix<NumType, CObsSize, CStateSize> H_dx;  // observation model
-  // Observation model.
-  H_dx = Matrix<NumType, Dynamic, Dynamic>::Zero(obsSize, stateSize);
+  H_dx.resize(obsSize, stateSize);
+  H_dx.setOnes();
 
   Matrix<NumType, CObsSize, CObsSize> R_meas;  // measurement noise
-  // Random symmetric matrix for measurement noise.
-  R_meas = Matrix<NumType, Dynamic, Dynamic>::Random(obsSize, obsSize);
-  R_meas = (R_meas.transpose() * R_meas).eval();
+  R_meas.resize(obsSize, obsSize);
+  std::default_random_engine eng(0);
+  for (int i = 0; i < obsSize; i++) {
+    for (int j = 0; j < obsSize; j++) {
+      R_meas(i, j) = int_distribution(eng);
+    }
+  }
+  R_meas = R_meas.transpose() * R_meas;
 
   // scratch space for computations
   Matrix<NumType, CStateSize, CObsSize> K_gain;
@@ -57,16 +66,60 @@ static void CovarianceUpdateEigen(benchmark::State &state) {
   }
 }
 
-#define GENERATE_EIGEN_DYNAMIC_AND_STATIC_FOR_SIZE(n)   \
+template <int CStateSize, int CObsSize = 3, typename NumType = double>
+static void CovarianceUpdateFastor(benchmark::State &state) {
+  using Fastor::inverse;
+  using Fastor::matmul;
+  using Fastor::Tensor;
+  using Fastor::transpose;
+
+  Tensor<NumType, CStateSize, CStateSize> P_x;
+  P_x.ones();
+  P_x = matmul(transpose(P_x), P_x);
+
+  Tensor<NumType, CObsSize, CStateSize> H_dx;
+  H_dx.ones();
+
+  Tensor<NumType, CObsSize, CObsSize> R_meas;
+  std::default_random_engine eng(0);
+  for (int i = 0; i < CObsSize; i++) {
+    for (int j = 0; j < CObsSize; j++) {
+      R_meas(i, j) = int_distribution(eng);
+    }
+  }
+  R_meas = Fastor::matmul(transpose(R_meas), R_meas);
+
+  Tensor<NumType, CStateSize, CObsSize> K_gain;
+  Tensor<NumType, CObsSize, CObsSize> S_res;
+  Tensor<NumType, CStateSize, CStateSize> I_KH;
+  Tensor<NumType, CStateSize, CStateSize> P_x_ref;
+
+  for (auto _ : state) {
+    S_res = matmul(matmul(H_dx, P_x), transpose(H_dx)) + R_meas;
+    K_gain = matmul(matmul(P_x, transpose(H_dx)), inverse(S_res));
+
+    I_KH.eye();
+    I_KH -= matmul(K_gain, H_dx);
+
+    P_x_ref = matmul(matmul(I_KH, P_x), transpose(I_KH));
+    P_x_ref += matmul(matmul(K_gain, R_meas), transpose(K_gain));
+
+    benchmark::DoNotOptimize(P_x_ref);
+    benchmark::ClobberMemory();
+  }
+}
+
+#define COVARIANCE_UPDATE_FOR_SIZE(n)                   \
+  BENCHMARK_TEMPLATE(CovarianceUpdateFastor, n);        \
   BENCHMARK_TEMPLATE(CovarianceUpdateEigen, n)->Arg(n); \
   BENCHMARK_TEMPLATE(CovarianceUpdateEigen, Eigen::Dynamic)->Arg(n);
 
-GENERATE_EIGEN_DYNAMIC_AND_STATIC_FOR_SIZE(8);
-GENERATE_EIGEN_DYNAMIC_AND_STATIC_FOR_SIZE(16);
-GENERATE_EIGEN_DYNAMIC_AND_STATIC_FOR_SIZE(32);
-GENERATE_EIGEN_DYNAMIC_AND_STATIC_FOR_SIZE(64);
-GENERATE_EIGEN_DYNAMIC_AND_STATIC_FOR_SIZE(128);
+COVARIANCE_UPDATE_FOR_SIZE(8);
+COVARIANCE_UPDATE_FOR_SIZE(16);
+COVARIANCE_UPDATE_FOR_SIZE(32);
+COVARIANCE_UPDATE_FOR_SIZE(64);
+COVARIANCE_UPDATE_FOR_SIZE(128);
 
-#undef GENERATE_EIGEN_DYNAMIC_STATIC_FOR_SIZE
+#undef COVARIANCE_UPDATE_FOR_SIZE
 
 BENCHMARK_MAIN();
